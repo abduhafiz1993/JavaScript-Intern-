3c52e59265526887cefbdf4e3cb654119a246942
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-02-27 10:48:43 -0800

    update Script2

---
+++ script2.js
@@ -0,0 +1,7 @@
+const number = 3.5;
+const thePower = 2;
+
+
+console.log(Math.pow(number , thePower)); // for power numbers
+console.log(Math.floor(number)); // for floor down for 2.3 be 2
+console.log(number*(Math.random())); // for choose 1 number  from 0-0.999999999
