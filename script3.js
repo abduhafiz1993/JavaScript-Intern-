a12e6929e55692aaba685d07dc9961262f9e18a8
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-02-27 10:53:45 -0800

    update on script3

---
+++ script3.js
@@ -0,0 +1,10 @@
+// Array
+
+var name = [2, 3, 5]
+
+console.log(name.include(3));
+console.log(name.include(8));
+name.push(6);
+console.log(name.length());
+name.pop();
+console.log(name.length());
