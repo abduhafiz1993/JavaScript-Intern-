392bfd864340cdb13b989e77f5d7a36682d766d8
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-02-27 10:41:41 -0800

    update on script file

---
+++ script.js
@@ -0,0 +1,7 @@
+slice( x, y ) // this a code which make our string cut from x index abtu y index not including y.
+ var name = "abdu" ; then
+alert(name.slice(0, 3)) // the out put of this is abd
+name=name.toUpperCase(); // this give ABDU
+name=name.toLowerCase(); // this give abdu
+var number= 2.7;
+alert(Maths.floor(number)) // this gives u floored down number 2
